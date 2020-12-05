22772dc31188b2d00124ae0159027cb04bae0db5
ghstk <ghstk@qq.com>
2020-12-05 21:00:20 +0800

    add GitTest

---
+++ src/com/ghstk/test/GitTest.java
@@ -0,0 +1,10 @@
+package com.ghstk.test;
+
+/**
+ * 2020/12/5 20:24
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("master - push1");
+    }
+}
